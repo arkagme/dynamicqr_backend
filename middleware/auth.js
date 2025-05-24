9003d6e10854ba2af3dbcfe147bdc3fc50b3f9fc
Arkaprava Gaine <codearka2002@gmail.com>
2025-05-24 19:45:38 +0530

    sync

--- middleware/auth.js
+++ middleware/auth.js
@@ -29,7 +29,7 @@ exports.setupPassport = function setupPassport(app){
 
     passport.serializeUser(function(user, cb) {
     process.nextTick(function() {
-        cb(null, { id: user.id, username: user.username, name: user.name , role: user.role });
+        cb(null, { id: user.id, username: user.username, name: user.name , role: user.role , email:user.email});
         });
     });
 
@@ -47,25 +47,21 @@ exports.setupPassport = function setupPassport(app){
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: callbackURL ,
-    scope: ['profile']
+    scope: ['profile','email']
   }, async function verify(issuer, profile, cb) {
     try {
-      // Check if user exists
       const result = await db.query(
         'SELECT * FROM federated_credentials WHERE provider = $1 AND subject = $2',
         [issuer, profile.id]
       );
       
       if (result.rows.length === 0) {
-        // Create new user
         const userResult = await db.query(
-          'INSERT INTO users (name) VALUES ($1) RETURNING id',
-          [profile.displayName]
+          'INSERT INTO users (name,email) VALUES ($1, $2) RETURNING id',
+          [profile.displayName , profile.emails[0].value]
         );
         
         const userId = userResult.rows[0].id;
-        
-        // Create credentials
         await db.query(
           'INSERT INTO federated_credentials (user_id, provider, subject) VALUES ($1, $2, $3)',
           [userId, issuer, profile.id]
@@ -73,7 +69,7 @@ exports.setupPassport = function setupPassport(app){
         
         return cb(null, { id: userId, name: profile.displayName });
       } else {
-        // Get existing user
+        // existing user
         const userResult = await db.query(
           'SELECT * FROM users WHERE id = $1',
           [result.rows[0].user_id]
